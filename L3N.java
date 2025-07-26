d3b6e94a229e9129cd9dafa6a4c60cde18b8ac71
ayush singh <145686342+ayush2005k@users.noreply.github.com>
2025-07-26 23:35:39 +0530

    Laregest of 3 Numbers
    Problem statement
    Write a program that takes three numbers a,b, and c as input and prints the largest number amongst them

---
+++ L3N.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class L3N {
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        System.out.println("Value of a is: " + a); // Added line to print value of a
+        int b = scanner.nextInt();
+        System.out.println("Value of b is: " + b); // Added line to print value of b
+        int c = scanner.nextInt();
+        System.out.println("Value of c is: " + c); // Added line to print value of c
+
+        int maxAB = Math.max(a, b);
+
+        int largestNumber = Math.max(maxAB, c);
+
+        System.out.println("max value is:" +  largestNumber);
+
+        scanner.close();
+    }
+}
