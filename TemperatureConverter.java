8f5b70b88e79143fee83036db4abb8a1aeb29169
ayush singh <145686342+ayush2005k@users.noreply.github.com>
2025-07-27 22:50:49 +0530

    Fahrenheit to Celsius
    Problem statement
    
    Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E), and Step Size (W), you need to convert all Fahrenheit values from Start to End at the gap of W into their corresponding Celsius values and print the table.
    
    Note:
    Print the floor of the Celsius values if they are non-negative else print its ceil value.

---
+++ TemperatureConverter.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class TemperatureConverter { // Class name changed to TemperatureConverter
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int S = sc.nextInt();
+        int E = sc.nextInt();
+        int W = sc.nextInt();
+
+        sc.close();
+
+        int currentFahrenheit = S;
+        while (currentFahrenheit <= E) {
+            double celsius = (currentFahrenheit - 32) * 5.0 / 9.0;
+            
+            int convertedCelsius;
+
+            if (celsius >= 0) {
+                convertedCelsius = (int) Math.floor(celsius);
+            } else {
+                convertedCelsius = (int) Math.ceil(celsius);
+            }
+            
+            System.out.println(currentFahrenheit + "\t" + convertedCelsius);
+            
+            currentFahrenheit += W;
+        }
+    }
+}
